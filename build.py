31b93c3b4ef38dbded90ef085093eed5b8c0bff2
Andrew Miranti <andrew.h.miranti@gmail.com>
2019-11-21 16:39:49 -0600

    Automatically generated lots of rules

--- build.py
+++ build.py
@@ -12,7 +12,17 @@ from kninja import *
 
 proj = KProject()
 
-michelson = proj.definition(alias="michelson", backend="llvm",main="test-michelson.k",runner_script="./michelson.py",other=["michelson.k", "michelson-syntax.k", "test-michelson-syntax.k"])
+files = [
+        # "test-michelson.k", covered by main=
+        "michelson.k", 
+        "michelson-syntax.k", 
+        "test-michelson-syntax.k", 
+        "michelson-annotation-syntax.k", 
+        "michelson-annotation-semantics.k", 
+        "michelson-config.k"
+]
+
+michelson = proj.definition(alias="michelson", backend="llvm",main="test-michelson.k",runner_script="./michelson.py",other=files)
 michelson.tests(inputs = glob("tests/unit/*.tz"))
 
 proj.main()
