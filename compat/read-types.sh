79f675067ddb6be45b4a0560e094aef5aff0f681
Andrew Miranti <andrew.h.miranti@gmail.com>
2020-02-07 17:58:11 -0600

    Data reading scripts

--- compat/read-types.sh
+++ compat/read-types.sh
@@ -1,1 +1,2 @@
-pcregrep -oM "(?<=\[ )@exitToken[^\\]]*"
+#!/bin/bash
+tezos-client typecheck script "$($(dirname $0)/contract-expander/run.sh $1)" --details 2>&1 | pcregrep -oM "(?<=\[ )@exitToken[^\\]]*" | sed 's/ : /\n/g'
