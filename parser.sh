d327a1f3e347b7d38de14d2c001152b6a3a975e3
Andrew Miranti <andrew.h.miranti@gmail.com>
2020-03-06 00:30:49 -0600

    Temporarily revert K version change due to issue 1145, fix broken unit test

--- parser.sh
+++ parser.sh
@@ -10,9 +10,9 @@ if which tezos-client>/dev/null 2>&1; then
     SUB_FILE="$TEMP_DIR/sub" ;
     EXPANDED_FILE="$TEMP_DIR/expanded"
     "$EXTRACTOR_DIR/run.sh" "$1" 'code_or_contract' 'true' | sed 's/({/{/g;s/})/}/g;s/true/True/g;s/false/False/g;s/.*<k>\(.*\)<\/k>.*/\1/;s/(\s*\(0x\w*\|[+-]*[0-9][0-9]*\|"\([^"]\|\\"\)*"\)\s*)/\1/g' > $CODE_FILE ;
-    paste -d'/' <(sed -E 's/\s+/\\s*/g;s/\(/\\s*(\\s*/g;s/\)/\\s*)\\s*/g;' $CODE_FILE) <(tezos-client expand macros in "$(cat $CODE_FILE)" 2>/dev/null | tr -d '\n') | sed -E 's|(.*)|s/\1/|' > $SUB_FILE ;
-    kast --directory "$SCRIPT_DIR" -o program "$1" | tr -d '\n' | sed -f $SUB_FILE > $EXPANDED_FILE ;
-    kast --directory "$SCRIPT_DIR" --expand-macros $EXPANDED_FILE ;
+    paste -d'/' <(sed -E 's/\s+/\\s*/g;s/\(/\\s*(\\s*/g;s/\)/\\s*)\\s*/g;' "$CODE_FILE") <(tezos-client expand macros in "$(cat $CODE_FILE)" 2>/dev/null | tr -d '\n') | sed -E 's|(.*)|s/\1/|' > "$SUB_FILE" ;
+    kast --directory "$SCRIPT_DIR" -o program "$1" | tr -d '\n' | sed -f "$SUB_FILE" > "$EXPANDED_FILE" ;
+    kast --directory "$SCRIPT_DIR" --expand-macros "$EXPANDED_FILE" || cat "$EXPANDED_FILE" > /dev/stderr ;
 else
     echo 'tezos-client not found, using normal parsing' >/dev/stderr ;
     kast "$1" --directory "$SCRIPT_DIR" --expand-macros ;
