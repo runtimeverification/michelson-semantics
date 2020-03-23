c20c0fbb79272b69408f35b52dce6f2aaf71f02b
Andrew Miranti <andrew.h.miranti@gmail.com>
2020-03-23 17:49:12 -0500

    Perform coverage check, fix bytes CONCAT where possible

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 source "$SCRIPT_DIRECTORY/common.sh"
-"$SCRIPT_DIRECTORY/compat/kompile-all.sh"
 kompile $KOMPILE_OPTS $* "$SCRIPT_DIRECTORY/unit-test.k"
+"$SCRIPT_DIRECTORY/compat/kompile-all.sh"
