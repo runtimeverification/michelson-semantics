e61299aa820baa74bd9e5a0cf4550417aa8f3943
Andrew Miranti <andrew.h.miranti@gmail.com>
2020-03-25 09:39:39 -0500

    Fixes to parser

--- parser.sh
+++ parser.sh
@@ -10,7 +10,7 @@ if which tezos-client>/dev/null 2>&1; then
     EXPANDED_FILE="$TEMP_DIR/expanded" ;
     "$EXTRACTOR_DIR/run.sh" "$1" 'code_or_contract' 'false' > "$GROUP_FILE" ;
     GROUP="$(grep -Eo '^\s*(code|contract)' "$GROUP_FILE")" ;
-    tezos-client 'expand' 'macros' 'in' "$(sed -E 's/^\s*(code|contract)\s*(.*);/\2/' "$GROUP_FILE")" | tr -d '\n' | sed -E "s/(.*)/$GROUP { \\1 } ;/" | cat - "$1" > "$EXPANDED_FILE" ;
+    tezos-client 'expand' 'macros' 'in' "$(sed -E 's/^\s*(code|contract)\s*(.*);/\2/' "$GROUP_FILE")" | tr -d '\n' | sed -E "s/(.*)/$GROUP {\\1} ;/;s/{{/{/g;s/}}/}/g" | cat - "$1" > "$EXPANDED_FILE" ;
     kast --directory "$SCRIPT_DIR" --expand-macros "$EXPANDED_FILE" || cat "$EXPANDED_FILE" > /dev/stdout;
 else
     echo 'tezos-client not found, using normal parsing' >/dev/stderr ;
