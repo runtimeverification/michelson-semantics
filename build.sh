4e1e21560956d22949a2522f4c3b617127114685
Dwight Guth <dwight.guth@runtimeverification.com>
2020-04-08 16:20:55 +0000

    use bison parser

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 source "$SCRIPT_DIRECTORY/common.sh"
-kompile $KOMPILE_OPTS $* "$SCRIPT_DIRECTORY/unit-test.k" && "$SCRIPT_DIRECTORY/compat/kompile-all.sh"
+kompile $KOMPILE_OPTS $* --gen-bison-parser "$SCRIPT_DIRECTORY/unit-test.k" && "$SCRIPT_DIRECTORY/compat/kompile-all.sh"
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 source "$SCRIPT_DIRECTORY/common.sh"
-krun --directory "$SCRIPT_DIRECTORY" --parser "$SCRIPT_DIRECTORY/parser.sh" $*
+TEMP_DIR="$(mktemp -d)" ;
+trap 'rm -rf "$TEMP_DIR"' EXIT ;
+"$SCRIPT_DIRECTORY/parser.sh" "$*" > "$TEMP_DIR/input.kore"
+llvm-krun -d "$SCRIPT_DIRECTORY/unit-test-kompiled" -c PGM "$TEMP_DIR/input.kore" Pgm korefile -o "$TEMP_DIR/output.kore" || cat "$TEMP_DIR/output.kore"
