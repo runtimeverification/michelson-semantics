50bf79a9042565c6cb5f5658e9c5c1d4288c9b3b
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2020-04-09 01:33:38 -0400

    fix ocaml setup on docker

--- build-deps.sh
+++ build-deps.sh
@@ -14,7 +14,11 @@ git submodule update --init --recursive
 cd "$K_DIRECTORY"
 mvn package -DskipTests
 cd "$TEZOS_DIRECTORY"
-opam init --bare --no-setup
+# ocaml setup
+# --bare               - do not install any base ocaml installation now
+# --no-setup           - do not ask to setup hooks
+# --disable-sandboxing - do not use bwrap which is not supported on our container
+opam init --bare --no-setup --disable-sandboxing
 make build-deps
 eval `opam env`
 make
