28ab9104b85bd571d857cd354dfc7994225b90d8
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-05-30 21:47:14 +0000

    run-tests, run-tests-ci: inline run-tests into run-tests-ci

--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -11,7 +11,16 @@ source "$SCRIPT_DIRECTORY/start.sh"
 set -u
 
 # run tests
-"$SCRIPT_DIRECTORY/run-tests.sh"
+FAILING_FILE="$SCRIPT_DIRECTORY/tests/failing.cross"
+TEST_DIR="$SCRIPT_DIRECTORY/tests/unit"
+
+for test in $(find $TEST_DIR -name '*.tzt'); do
+  ! grep "${test#$TEST_DIR}" "$FAILING_FILE" &> /dev/null || continue
+  # run test and get actual return value
+  echo "Cross Validating: $test"
+  cross_validate_output='0'
+  $SCRIPT_DIRECTORY/cross-validate.sh "$test"
+done
 
 # wind down tests
 "$SCRIPT_DIRECTORY/shutdown.sh"
