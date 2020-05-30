1e0aca08ae8cde28fd52a93f99c1e98eba9ed75a
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-05-30 23:16:55 +0000

    run-with-tezos.sh, start.sh, shutdown.sh: inline scripts into run-with-tezos.sh

--- run-with-tezos.sh
+++ run-with-tezos.sh
@@ -1,22 +1,44 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+shopt -s expand_aliases
 
 notif() { echo "== $@" >&2 ; }
 fatal() { echo "[FATAL] $@" ; exit 1 ; }
 
-command="$1" ; shift
-
 SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+TEZOS_DIR="$(readlink -f "$SCRIPT_DIRECTORY/ext/tezos")"
+export PATH="$TEZOS_DIR:$PATH"
 
-# do test initialization
-set +u
-source "$SCRIPT_DIRECTORY/start.sh"
-set -u
+TEZOS_NODE_PID=
 
-# run tests
+start_tezos() {
+    "$TEZOS_DIR/src/bin_node/tezos-sandboxed-node.sh" 1 --connections 1 >/dev/null 2>&1 &
+    TEZOS_NODE_PID=$!
+    sleep 5
+
+    if ! kill -0 "$TEZOS_NODE_PID" >/dev/null 2>&1 ; then
+        echo 'There seems to already be a tezos sandboxed node running, exitting' ;
+        kill -2 $$ # https://stackoverflow.com/questions/6112540/return-an-exit-code-without-closing-shell
+    fi
+
+    set +u
+    eval $("$TEZOS_DIR/src/bin_client/tezos-init-sandboxed-client.sh" 1)
+    set -u
+
+    tezos-activate-alpha
+    tezos-autocomplete
+}
+
+stop_tezos() {
+    kill -15 "$TEZOS_NODE_PID"
+}
+
+start_tezos
+
 FAILING_FILE="$SCRIPT_DIRECTORY/tests/failing.cross"
 TEST_DIR="$SCRIPT_DIRECTORY/tests/unit"
+command="$1" ; shift
 
 for test in $(find $TEST_DIR -name '*.tzt'); do
   ! grep "${test#$TEST_DIR}" "$FAILING_FILE" &> /dev/null || continue
@@ -30,5 +52,4 @@ for test in $(find $TEST_DIR -name '*.tzt'); do
   esac
 done
 
-# wind down tests
-"$SCRIPT_DIRECTORY/shutdown.sh"
+stop_tezos
--- shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-NODE_PID="$(dirname "$(readlink -f "$BASH_SOURCE")")/.node-pid"
-kill -15 "$(cat $NODE_PID)"
-rm "$NODE_PID"
