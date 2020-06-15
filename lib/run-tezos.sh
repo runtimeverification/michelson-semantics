52aa066d6c2643d5ce1fd53ca62c4ee410cb99a6
Nishant Rodrigues <nishantjr@gmail.com>
2020-06-15 11:19:08 -0500

    Closes #50 : Use tezos mock client and cleanup (#52)
    * lib/tezos-client-unit-test: Merge several recipes and the run-with tesoz script
    
    This is slower, but much easier to work with. The five second sleep will be removed
    when moving to the mockup client
    
    * lib/tezos-client-unit-test: Use mock client; share TEMP_DIR with fix-address and run-rezos
    
    * lib/tezos-client-unit-test: Inline fix-address
    
    * run-tezos: Remove duplicated code
    
    * Remove unused parser.sh
    
    * Move various scripts for cross tests into lib/
    
    * fix bug in fix_address() function
    
    * update failing.cross to reflect current test coverage
    
    * remove tezos_client function definition
    
    Co-authored-by: Stephen Skeirik <stephen.skeirik@runtimeverification.com>

--- lib/run-tezos.sh
+++ lib/run-tezos.sh
@@ -21,21 +21,6 @@ extract() {
     python3 "$SCRIPT_DIR/extract-group.py" "$test_file_extracted" "$@"
 }
 
-TEMP_DIR="$SCRIPT_DIR/.failure"
-
-rm -rf "$TEMP_DIR"
-mkdir -p "$TEMP_DIR"
-
-KNOWN_FAKES="$SCRIPT_DIR/addresses.txt"
-
-FOUND_ADDRESSES="$TEMP_DIR/addresses"
-REAL_ADDRESSES_UNSORTED="$TEMP_DIR/contracts_unsorted"
-REAL_ADDRESSES="$TEMP_DIR/contracts"
-FAKE_ADDRESSES="$TEMP_DIR/fake_addresses"
-FAKE_ADDRESS_SUBS="$TEMP_DIR/fake_address_subs"
-ORIGINATION_OUTPUTS="$TEMP_DIR/originations"
-ORIGINATION_SUBS="$TEMP_DIR/origination_subs"
-ALL_SUBS="$TEMP_DIR/subs"
 TYPECHECK_OUTPUT="$TEMP_DIR/typecheck"
 RAW_TYPES="$TEMP_DIR/rawtypes"
 TYPES_FILE="$TEMP_DIR/types"
@@ -47,46 +32,7 @@ FIXED_ADDRS_OUTPUT="$TEMP_DIR/others_fixed"
 REAL_OUTPUT_FILE="$TEMP_DIR/actual"
 EXPECTED_OUTPUT_FILE="$TEMP_DIR/expected"
 
-if ! grep -o '@Address([^)"]*)' "$test_file" | sort | uniq > "$FOUND_ADDRESSES"; then
-    touch "$FOUND_ADDRESSES"
-fi
-
-extract other_contracts true | tr -d '{}' | tr ';' '\n' | sed -E 's/^\s*//;s/\s*$//;s/Elt\s*"([^"]*)"\s*(.*)/\1#\2/;/^\s*$/d' > "$REAL_ADDRESSES_UNSORTED"
-
-FAKE_SENDER=$(extract sender true)
-if [ ! -z $FAKE_SENDER ] && ! ( grep "$FAKE_SENDER" "$REAL_ADDRESSES_UNSORTED" ); then
-    echo "$FAKE_SENDER#unit" >> "$REAL_ADDRESSES_UNSORTED"
-fi
-
-FAKE_SOURCE=$(extract source true)
-if [ ! -z $FAKE_SOURCE ] && ! ( grep "$FAKE_SOURCE" "$REAL_ADDRESSES_UNSORTED" ); then
-    echo "$FAKE_SOURCE#unit" >> "$REAL_ADDRESSES_UNSORTED"
-fi
-
-FAKE_SELF=$(extract self true)
-REAL_SELF="$(tezos-client run script "parameter unit ; storage (option address) ; code { DROP ; SELF ; ADDRESS ; CONTRACT unit ; IF_SOME { ADDRESS ; SOME ; NIL operation ; PAIR } {FAIL} }" on storage None and input Unit 2>&1 | grep "Some" | sed -E 's/\s*\(Some "([^"]*)"\)\s*/\1/')"
-
-sort "$REAL_ADDRESSES_UNSORTED" | uniq > "$REAL_ADDRESSES"
-
-diff --new-line-format="" --unchanged-line-format="" "$FOUND_ADDRESSES" <(cut -d'#' -f 1 "$REAL_ADDRESSES") > "$FAKE_ADDRESSES"
-
-if [ ! -z "$FAKE_SELF" ] ; then
-    sed -i "/$FAKE_SELF/d" "$FAKE_ADDRESSES" ;
-fi
-
-paste -d'/' "$FAKE_ADDRESSES" <(head -n "$(wc -l "$FAKE_ADDRESSES" | grep -o "^[0-9]*")" $KNOWN_FAKES  ) | sed -E 's|(.*)|s/\1/|' > "$FAKE_ADDRESS_SUBS"
-
-if [ ! -z $FAKE_SELF ] ; then
-    echo "s|$FAKE_SELF|$REAL_SELF|" >> "$FAKE_ADDRESS_SUBS"
-fi
-
-python3 $SCRIPT_DIR/originate.py $REAL_ADDRESSES > $ORIGINATION_OUTPUTS \
-    || fatal "Failed to originate contracts: $test_file"
-paste -d '/' <(cut -d'#' -f1 "$REAL_ADDRESSES") <(grep -Po '(?<=New contract )[a-zA-Z0-9_]*' "$ORIGINATION_OUTPUTS") | sed -E 's|(.*)|s/\1/|;s|s///||' > "$ORIGINATION_SUBS"
-
-cat "$FAKE_ADDRESS_SUBS" "$ORIGINATION_SUBS" > "$ALL_SUBS"
-
-tezos-client typecheck script "$(cat $test_file_expanded)" --details  > $TYPECHECK_OUTPUT 2>&1 \
+$TEZOS_CLIENT typecheck script "$(cat $test_file_expanded)" --details  > $TYPECHECK_OUTPUT 2>&1 \
     || fatal "Contract did not typecheck: $test_file"
 
 pcregrep -oM '(?<=\[)\s*@exitToken[^]]*' "$TYPECHECK_OUTPUT" > "$RAW_TYPES"
@@ -111,7 +57,7 @@ if [ ! -z "$FAKE_SENDER" ] ; then
     fi ;
 fi
 
-tezos-client run script "$(cat $test_file_expanded)" on storage Unit and input "$(cat "$test_file_input")" --amount "$AMOUNT" --trace-stack "${TRACE_CLI[@]}" > "$EXECUTION" 2>&1 || true
+$TEZOS_CLIENT run script "$(cat $test_file_expanded)" on storage Unit and input "$(cat "$test_file_input")" --amount "$AMOUNT" --trace-stack "${TRACE_CLI[@]}" > "$EXECUTION" 2>&1 || true
 # For some reason, the cli argument for "SENDER" is "--source" and "SOURCE" is "--payer"
 
 pcregrep -oM '(?<=\[)\s*Unit\s*@exitToken[^]]*' "$EXECUTION" > "$RAW_DATA"
