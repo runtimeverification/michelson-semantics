f08493296ed8212f2d0c8240e6ee012b833f76fb
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2021-10-12 19:35:18 -0400

    add test harness to tezos-utils

---
+++ ext/tezos-utils/scripts/run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+MAIN_DIR="$SELF_DIR/.."
+TESTS_DIR="$MAIN_DIR/tests"
+INPUT_DIR="$TESTS_DIR/input"
+OUTPUT_DIR="$TESTS_DIR/output"
+
+tezos_utils="$MAIN_DIR/tezos-utils"
+
+BUILD_TESTS=
+[ $# -eq 1 ] && [ "$1" == "build" ] && BUILD_TESTS=on
+
+for test in $(ls -- "$INPUT_DIR"); do
+  testname=$(basename -s .tz "$test")
+  testin="$INPUT_DIR/$testname.tz"
+  testout="$OUTPUT_DIR/$testname.dot.testout"
+  testexp="$OUTPUT_DIR/$testname.dot"
+  if [ -n "$BUILD_TESTS" ]; then
+    python "$tezos_utils" convert -i michelson -o dot "$testin" "$testexp"
+  else
+    python "$tezos_utils" convert -i michelson -o dot "$testin" "$testout"
+    diff -q "$testout" "$testexp"
+  fi
+done
